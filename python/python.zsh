b1d7939bfb1d09cc0bf12715256dc1d656a4a3e2
Dan Palmer <dan@danpalmer.me>
2016-01-04 12:18:53 +0000

    Set up pyenv virtualenvwrapper

--- python/python.zsh
+++ python/python.zsh
@@ -1,13 +1,7 @@
 alias rmpycs="find . -name '*.pyc' -delete"
 
-export WORKON_HOME=$HOME/.virtualenvs
-
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 export PYENV_ROOT=/usr/local/var/pyenv
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 
-if [[ -r /usr/local/bin/virtualenvwrapper.sh ]]; then
-    source /usr/local/bin/virtualenvwrapper.sh
-else
-    echo "WARNING: Can't find virtualenvwrapper.sh"
-fi
+pyenv virtualenvwrapper
