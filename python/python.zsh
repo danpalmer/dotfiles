2ee5b3bbb97672bc055fd8b016063f30ee224b2f
Dan Palmer <dan@danpalmer.me>
2016-03-27 11:59:05 +0100

    Disable venv prompt, I have my own

--- python/python.zsh
+++ python/python.zsh
@@ -3,5 +3,6 @@ alias rmpycs="find . -name '*.pyc' -delete"
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 export PYENV_ROOT=/usr/local/var/pyenv
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 pyenv virtualenvwrapper
