a43338ca4a4b767eb9d5b48db4fc7430ec9cc620
Dan Palmer <dan@danpalmer.me>
2016-09-13 18:16:36 +0100

    Fix Python, let's not use Pyenv

--- python/python.zsh
+++ python/python.zsh
@@ -1,9 +1,6 @@
 alias rmpycs="find . -name '*.pyc' -delete"
 
-export PYENV_ROOT=/usr/local/var/pyenv
-export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 export VIRTUAL_ENV_DISABLE_PROMPT=1
-
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-
-pyenv virtualenvwrapper
+export WORKON_HOME=~/.virtualenvs
+mkdir -p $WORKON_HOME
+source /usr/local/bin/virtualenvwrapper.sh
