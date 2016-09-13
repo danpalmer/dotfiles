c1c96ef6b9da25d7a53da0a589f39ad6d341ada7
Dan Palmer <dan@danpalmer.me>
2016-09-13 18:16:41 +0100

    Handy

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias reload='. ~/.zshrc'
 alias jsc="/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc"
+alias gitcleanup="git branch --merged | grep -v "\*" | xargs -n 1 git branch -d"
