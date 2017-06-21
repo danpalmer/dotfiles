e5e9cb2f01773afeeb39dc234289316a282d77ff
Dan Palmer <dan@danpalmer.me>
2017-06-21 14:40:50 +0100

    Use homebrew python

--- python/python.zsh
+++ python/python.zsh
@@ -1,6 +1,7 @@
 alias rmpycs="find . -name '*.pyc' -delete"
 
 export VIRTUAL_ENV_DISABLE_PROMPT=1
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
 export WORKON_HOME=~/.virtualenvs
 mkdir -p $WORKON_HOME
 source /usr/local/bin/virtualenvwrapper.sh
