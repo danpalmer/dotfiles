3b34d0591db9afe8e17309ef0be7fac3c165cd14
Dan Palmer <dan@danpalmer.me>
2017-06-21 14:41:02 +0100

    Improve aliases

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,9 +1,9 @@
-alias rs="styleme/manage.py runserver"
+alias rs="styleme/manage.py runserver 0.0.0.0:8000"
 alias mt="styleme/manage.py test"
 alias mm="styleme/manage.py migrate"
 alias mmm="styleme/manage.py makemigrations"
 alias tp="styleme/manage.py test --failfast && git push"
-alias ms="styleme/manage.py shell"
+alias ms="styleme/manage.py shell_plus --ipython"
 alias gr="styleme/manage.py generate_reports"
 
 alias tox="tox --tox-pyenv-no-fallback"
