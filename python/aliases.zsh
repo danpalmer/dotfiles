386ce9dc7582aaf04b5db5c7b95af2b2182ebd99
Dan Palmer <dan@danpalmer.me>
2017-09-23 12:56:37 +0100

    Update

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -5,3 +5,5 @@ alias mmm="styleme/manage.py makemigrations"
 alias tp="styleme/manage.py test --failfast && git push"
 alias ms="styleme/manage.py shell_plus --ipython"
 alias gr="styleme/manage.py generate_reports"
+
+alias withbrew="CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib"
