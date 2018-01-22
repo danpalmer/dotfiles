91ecf03d72a8d02e28f8cfe4d516091f39220f70
Dan Palmer <dan@danpalmer.me>
2018-01-22 09:48:25 +0000

    Add shortcut for getting the git hash

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,7 +6,6 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
@@ -17,6 +16,7 @@ alias gcd='git checkout -'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gap='git add -p'
+alias gh='git rev-parse HEAD | pbcopy'
 
 move_commits_to_new_branch() {
     git checkout -b $1
