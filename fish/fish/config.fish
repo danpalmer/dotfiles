68b801e60b2b5b8b137f123c5f624b766bb45acb
Dan Palmer <dan@danpalmer.me>
2018-07-31 14:21:18 +0100

    Move these to the path

---
+++ fish/fish/config.fish
@@ -0,0 +1,5 @@
+set -U fish_user_paths \
+    /usr/local/heroku/bin \
+    `yarn global bin` \
+    $HOME/.nvm \
+    $HOME/.yarn/bin
--- node/node.zsh
+++
@@ -1,6 +0,0 @@
-export PATH="`yarn global bin`:$PATH"
-
-export NVM_DIR="$HOME/.nvm"
-source "/usr/local/opt/nvm/nvm.sh"
-
-export PATH="$HOME/.yarn/bin:$PATH"
