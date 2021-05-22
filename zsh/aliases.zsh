af68a1787a8c80547b5bc5a6a5dd517cd0fc617f
Dan Palmer <dan@thread.com>
2021-05-22 17:41:41 +0100

    Drop old stuff

--- zsh/aliases.zsh
+++
@@ -1,2 +0,0 @@
-alias reload='. ~/.zshrc'
-alias jsc="/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc"
--- zsh/prompt.zsh
+++
@@ -1,8 +0,0 @@
-autoload -U colors && colors
-
-function virtualenv_prompt_info(){
-  [[ -n ${VIRTUAL_ENV} ]] || return
-  echo "[${VIRTUAL_ENV:t}]"
-}
-
-PROMPT='$(virtualenv_prompt_info)%{$fg[magenta]%}[%c]%{$reset_color%}$(git-radar --zsh --fetch) '
