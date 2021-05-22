bd6b87f592c548764c23b98699034ed1da762e34
Dan Palmer <dan@thread.com>
2021-05-22 17:46:34 +0100

    No longer managed here

--- python/aliases.zsh
+++
@@ -1,9 +0,0 @@
-alias rs="styleme/manage.py runserver 0.0.0.0:8000"
-alias mt="styleme/manage.py test"
-alias mm="styleme/manage.py migrate"
-alias mmm="styleme/manage.py makemigrations"
-alias tp="styleme/manage.py test --failfast && git push"
-alias ms="styleme/manage.py shell_plus --ipython"
-alias gr="styleme/manage.py generate_reports"
-
-alias withbrew="CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib"
--- python/python.zsh
+++
@@ -1,7 +0,0 @@
-alias rmpycs="find . -name '*.pyc' -delete"
-
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export WORKON_HOME=~/.virtualenvs
-mkdir -p $WORKON_HOME
-source /usr/local/bin/virtualenvwrapper.sh
