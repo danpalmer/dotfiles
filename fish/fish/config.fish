d8704a2a16469e7425bafc2131b86bff1e79c7d5
Dan Palmer <dan@danpalmer.me>
2025-04-17 17:19:44 +1000

    Misc fish updates
    Drops pyenv stuff, we’ll use uv for that.

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -1,46 +1,29 @@
+# iTerm
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+# NVM
 set nvm_prefix /usr/local/opt/nvm/
 
-set -U fish_user_paths \
-    `yarn global bin` \
-    $HOME/.nvm \
-    $HOME/.local/bin \
-    $HOME/.cargo/bin \
-    /usr/local/opt/gettext/bin \
-    /Applications/Postgres.app/Contents/Versions/latest/bin
+# Homebrew
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
+
+# Bun JS
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# Ruby
+status --is-interactive; and source (rbenv init - fish | psub)
+
+# OrbStack
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 
 function fish_prompt
-    if set -q VIRTUAL_ENV
-        printf '%s[%s]%s %s[%s]%s$ ' \
-            (set_color -b blue white) \
-            (basename "$VIRTUAL_ENV") \
-            (set_color normal) \
-            (set_color $fish_color_cwd) \
-            (prompt_pwd) \
-            (set_color normal)
-
-    else
-        printf '%s[%s]%s$ ' \
-            (set_color $fish_color_cwd) \
-            (prompt_pwd) \
-            (set_color normal)
-    end
+    printf '%s[%s]%s$ ' \
+        (set_color $fish_color_cwd) \
+        (prompt_pwd) \
+        (set_color normal)
 end
-
-status --is-interactive; and source (rbenv init -|psub)
-
-function on-python-diff
-    git diff --name-only master | grep ".py\$" | xargs $argv
-end
-
-status --is-interactive; and pyenv init - | source
-status --is-interactive; and pyenv virtualenv-init - | source
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
 function fish_greeting
 end
-
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-starship init fish | source
-
-eval (env _T_COMPLETE=fish_source t)
